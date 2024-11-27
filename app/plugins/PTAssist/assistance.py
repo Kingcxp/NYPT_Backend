6df20cae7d9ae507a216f48cbd87702f03dcc0b2
Kingcq <404291187@qq.com>
2024-11-27 21:20:00 +0800

    fix: making dir to prevent path not exist problem

--- app/plugins/PTAssist/assistance.py
+++ app/plugins/PTAssist/assistance.py
@@ -120,6 +120,8 @@ async def upload_roomdata(item: UploadRoomdataItem, request: Request, db: AsyncS
             "msg": "会场令牌不正确！"
         }, status_code=status.HTTP_400_BAD_REQUEST)
     try:
+        if not os.path.exists(Config.TEMP_FOLDER):
+            os.makedirs(Config.TEMP_FOLDER)
         filepath = os.path.join(
             Config.TEMP_FOLDER,
             Config.TEMP_FILE_NAME.format(
